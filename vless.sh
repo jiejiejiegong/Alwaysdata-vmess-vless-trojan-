9372301f50632de58c80f10cef214a37541d14d9
jiejiejiegong <61736425+jiejiejiegong@users.noreply.github.com>
2022-10-20 14:00:40 +0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -5,7 +5,7 @@ wget -N https://raw.githubusercontent.com/jiejiejiegong/Alwaysdata-vmess-vless-t
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="4fd46ad4-3272-4f3c-af05-241a15528f69"
+    id="4fd46ad4-3272-4f3c-af05-241a14428f69"
 fi
 
 cat <<EOF > ~/config.json
